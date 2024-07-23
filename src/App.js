7c650093cfaeeed14d6c7145c7dfbc418f985172
borgssam <borgssam@gmail.com>
2024-07-23 17:13:26 +0900

    V14 메뉴(좌측메뉴 적용)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import './styles/App.css';
 import {TopArea} from './componetes/TopArea';
 import {BodyArea} from './componetes/BodyArea';
 import {BottomArea} from './componetes/BottomArea';
 
 function App() {
+  const [leftMenu, setLeftMenu] = useState(4);
+
   return (
     <div className="App">
       <div id="site">
-        <TopArea/>
-        <BodyArea/>
+        <TopArea setLeftMenu={setLeftMenu} />
+        <BodyArea leftMenu={leftMenu} />
         <BottomArea/>
       </div>
     </div>
